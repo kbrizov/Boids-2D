2f618bd2d3ed7e5c87c7ac07862f33136606103f
Christian Rizov <k.b.rizov@gmail.com>
2020-09-03 16:24:30 +0300

    1. Extended the Flock script and renamed it to Swarm (because I'm dramatic like that). 2. Extended the BoidPerceptionComponent script. 3. Created a Swarm prefab.

--- Assets/Scripts/BoidPerceptionComponent.cs
+++ Assets/Scripts/BoidPerceptionComponent.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
 
 public class BoidPerceptionComponent : MonoBehaviour
 {
@@ -7,4 +9,16 @@ public class BoidPerceptionComponent : MonoBehaviour
 
     [SerializeField]
     private PerceptionCollider2D m_outerPerception = null;
+
+    public IEnumerable<Transform> GetInnerPerceivedObjects()
+    {
+        Assert.IsTrue(m_innerPerception != null);
+        return m_innerPerception.GetPerceivedObjects();
+    }
+
+    public IEnumerable<Transform> GetOuterPerceivedObjects()
+    {
+        Assert.IsTrue(m_outerPerception != null);
+        return m_outerPerception.GetPerceivedObjects();
+    }
 }
---
+++ Assets/Scripts/Swarm.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Swarm : MonoBehaviour
+{
+    private readonly IList<Boid> m_boids = new List<Boid>();
+
+    [SerializeField]
+    [Range(0.0f, 256)]
+    private uint numberOfBoids = 64;
+
+    [SerializeField]
+    [Range(0.0f, 512)]
+    private uint spawnAreaRadius = 8;
+
+    [SerializeField]
+    private Boid m_boidPrefab = null;
+
+    [SerializeField]
+    private BoidBehaviour m_boidBehaviour = null;
+
+    public void Start()
+    {
+        this.Initialize();
+    }
+
+    private void Initialize()
+    {
+        for (int i = 0; i < numberOfBoids; i++)
+        {
+            Vector2 position = Random.insideUnitCircle * spawnAreaRadius;
+            Quaternion rotation = Quaternion.Euler(Vector3.forward * Random.Range(0, 360));
+            Boid boid = Instantiate(m_boidPrefab, position, rotation, parent: this.transform);
+
+            m_boids.Add(boid);
+        }
+    }
+}
