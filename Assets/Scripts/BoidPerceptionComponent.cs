090512f27d739d69781a684b5e5e96d933c3c99c
Christian Rizov <k.b.rizov@gmail.com>
2020-09-11 10:04:58 +0300

    Refactoring the PerceptionComponent: 1. Returned the optimization code from yesterday. I got confused that it does not grant me performance gains. I was wrong. 2. Added a check for the self collider. Opted out from using an if statement in the foreach loop because my benchmarks showed that it's a lot slower.

--- Assets/Scripts/BoidPerceptionComponent.cs
+++ Assets/Scripts/BoidPerceptionComponent.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 
 public class BoidPerceptionComponent : MonoBehaviour
 {
+    private const int INITIAL_PERCEPTION_CAPACITY = 128;
+
+    private ICollection<GameObject> m_innerPerceivedObjects = new List<GameObject>(INITIAL_PERCEPTION_CAPACITY);
+    private ICollection<GameObject> m_outerPerceivedObjects = new List<GameObject>(INITIAL_PERCEPTION_CAPACITY);
+
     [SerializeField]
     private float m_innerPerceptionRadius = 1.0f;
 
@@ -14,23 +19,30 @@ public class BoidPerceptionComponent : MonoBehaviour
 
     public ICollection<GameObject> GetInnerPerceivedObjects()
     {
-        return this.GetPerceivedObjectsInRadius(m_innerPerceptionRadius);
+        this.GetPerceivedObjectsInRadius(m_innerPerceptionRadius, ref m_innerPerceivedObjects);
+
+        return m_innerPerceivedObjects;
     }
 
     public ICollection<GameObject> GetOuterPerceivedObjects()
     {
-        return this.GetPerceivedObjectsInRadius(m_outerPerceptionRadius);
+        this.GetPerceivedObjectsInRadius(m_outerPerceptionRadius, ref m_outerPerceivedObjects);
+
+        return m_outerPerceivedObjects;
     }
 
-    private ICollection<GameObject> GetPerceivedObjectsInRadius(float radius)
+    private ICollection<GameObject> GetPerceivedObjectsInRadius(float radius, ref ICollection<GameObject> perceivedObjects)
     {
+        perceivedObjects.Clear();
+
         Collider2D[] perceivedColliders = Physics2D.OverlapCircleAll(this.transform.position, radius, m_perceptionLayer);
-        ICollection<GameObject> perceivedObjects = new List<GameObject>(perceivedColliders.Length);
 
         foreach (var collider in perceivedColliders)
         {
             perceivedObjects.Add(collider.gameObject);
         }
+
+        perceivedObjects.Remove(this.gameObject);
 
         return perceivedObjects;
     }
