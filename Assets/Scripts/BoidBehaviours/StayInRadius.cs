084fdd33910fc0d6aa1c339257345d44e73c9f34
Christian Rizov <k.b.rizov@gmail.com>
2020-09-15 22:47:49 +0300

    1. Implemented a new behaviour StayInRadius. 2. Added it to the Composite (Default) behaviour. 3. Modified the Boid prefab movement speed.

---
+++ Assets/Scripts/BoidBehaviours/StayInRadius.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Scripts.BoidBehaviours
+{
+    [CreateAssetMenu(menuName = "Boid Behaviour/Stay In Radius")]
+    public class StayInRadius : BoidBehaviour
+    {
+        [SerializeField]
+        private Vector2 m_center = Vector2.zero;
+
+        [SerializeField]
+        [Range(0.0f, 100.0f)]
+        private float m_radius = 10.0f;
+
+        public override Vector2 CalculateMove(Boid boid)
+        {
+            Assert.IsTrue(boid != null);
+
+            Vector2 directionToCenter = m_center - (Vector2)boid.transform.position;
+            float distanceToCenter = directionToCenter.magnitude;
+
+            if (m_radius < distanceToCenter)
+            {
+                return directionToCenter.normalized;
+            }
+
+            return Vector2.zero;
+        }
+    }
+}
